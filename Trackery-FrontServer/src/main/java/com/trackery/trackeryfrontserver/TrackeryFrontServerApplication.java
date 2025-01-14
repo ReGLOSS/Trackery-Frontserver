199ffcaea5e30657d860d75a7405a9bc87e0479d
한제우 <hanjaewoo98@gmail.com>
2025-01-14 22:09:16 +0900

    initial commit

---
+++ Trackery-FrontServer/src/main/java/com/trackery/trackeryfrontserver/TrackeryFrontServerApplication.java
@@ -0,0 +1,13 @@
+package com.trackery.trackeryfrontserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrackeryFrontServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrackeryFrontServerApplication.class, args);
+	}
+
+}
---
+++ Trackery-FrontServer/src/test/java/com/trackery/trackeryfrontserver/TrackeryFrontServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trackery.trackeryfrontserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrackeryFrontServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
