7da7ff7fdbbad72d6f76f66adf4085ac4f1e8013
durururuk <147094944+durururuk@users.noreply.github.com>
2025-03-27 16:56:24 +0900

    feature/메인페이지/베이스/ 주석 수정

--- src/main/java/com/trackery/trackeryfrontserver/config/AuthFilter.java
+++ src/main/java/com/trackery/trackeryfrontserver/config/AuthFilter.java
@@ -52,11 +52,12 @@ public class AuthFilter extends OncePerRequestFilter {
 	private static final String AUTH_ENDPOINT = "/api/auth/me";
 
 	/**
-	 * 클라이언트가 인증이 필요한 URI에 접근할 때 인증 여부를 반환하는 백엔드 API를 호출해서
+	 * 클라이언트가 인증이 필요한 URI에 접근할 때,
+	 * 백엔드 API를 호출해서 인증 정보를 확인하고
 	 * 인증 정보를 시큐리티 컨텍스트에 담는 필터입니다.
 	 *
-	 * @param request : 요청 HttpServletRequest
-	 * @param response : 응답 HttpServletResponse
+	 * @param request : 클라이언트 요청 객체 (HttpServletRequest)
+	 * @param response : 서버 응답 객체 (HttpServletResponse)
 	 * @param filterChain : 인증 필요한 URI에서 작동하는 시큐리티 필터체인
 	 */
 	@Override
@@ -89,9 +90,9 @@ public class AuthFilter extends OncePerRequestFilter {
 	}
 
 	/**
-	 * request servlet에서 쿠키를 파싱해서 액세스 토큰 쿠키를 Optional로 반환하는 메서드
+	 * 요청 객체에서 쿠키를 파싱해서 액세스 토큰 쿠키를 Optional로 반환하는 메서드
 	 *
-	 * @param request : 요청 HttpServletRequest
+	 * @param request : 클라이언트 요청 객체 (HttpServletRequest)
 	 * @return : 액세스 토큰 쿠키
 	 */
 	private Optional<Cookie> extractAccessTokenCookie(HttpServletRequest request) {
@@ -119,11 +120,12 @@ public class AuthFilter extends OncePerRequestFilter {
 
 	/**
 	 * ResponseEntity의 body를 파싱해서 인증 정보를 Spring Security Context Holder에 저장합니다.
-	 * 단순 인증 필터지만 userName, userRoldId를 받는 이유는 시큐리티 컨텍스트 홀더에 담아서
+	 * 단순 인증 필터지만 userName, userRoleId를 받는 이유는 시큐리티 컨텍스트 홀더에 담아서
 	 * 다음 필터에도 인증 정보를 넘기기 위해서는 인증 정보가 필요합니다.
-	 * userRole에 따른 페이지 접근도 관리할 일이 있을 것 같아서 userRoleId도 함께 저장합니다.
+	 * userRoleId는 사용자 권한(Role)에 따른 페이지 접근을 관리하기 위해 저장합니다.
  	 *
-	 * @param body : 백엔드서버에서 보내준 응답의 body를 String으로 저장한 객체
+	 * @param body : 백엔드서버에서 반환된 응답의 body를 String으로 저장한 객체입니다.
+	 *               JSON 형식, userId, userName, userRoleId 포함
 	 * @throws JsonProcessingException : Json 프로세싱 시 던져질 수 있는 예외
 	 */
 	private void createAuthentication(String body) throws JsonProcessingException {
