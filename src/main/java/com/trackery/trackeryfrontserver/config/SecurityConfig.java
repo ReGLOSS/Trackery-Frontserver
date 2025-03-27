9c173ca440748222d8a8656ac0448f58cb645e93
durururuk <147094944+durururuk@users.noreply.github.com>
2025-03-27 15:56:08 +0900

    feature/메인페이지/베이스/ publicUri와 protectedUri를 관리하는 필터체인 분리

--- src/main/java/com/trackery/trackeryfrontserver/config/SecurityConfig.java
+++ src/main/java/com/trackery/trackeryfrontserver/config/SecurityConfig.java
@@ -2,10 +2,16 @@ package com.trackery.trackeryfrontserver.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import com.trackery.trackeryfrontserver.domain.proxy.service.ProxyService;
+
+import lombok.RequiredArgsConstructor;
 
 /**
  * packageName    : com.trackery.trackeryfrontserver.config
@@ -20,9 +26,12 @@ import org.springframework.security.web.SecurityFilterChain;
  */
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class SecurityConfig {
+	private final ProxyService proxyService;
 
-	private final String[] publicUris = {"/register/**", "/login/**"};
+	private static final String[] PUBLIC_URIS = {"/register/**", "/login/**", "/", "/resources/**", "/css/**", "/js/**",
+		"/images/**", "/error", "/api/**", "/icons/**", "/module/**", "/error"};
 
 	/**
 	 * Spring Security 필터 체인을 구성합니다.
@@ -32,14 +41,27 @@ public class SecurityConfig {
 	 * @throws Exception 보안 구성 중 발생할 수 있는 예외
 	 */
 	@Bean
+	@Order(1)
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+		http
+			.securityMatcher(PUBLIC_URIS)
+			.csrf(AbstractHttpConfigurer::disable)
+			.authorizeHttpRequests(auth -> auth.anyRequest().permitAll())
+			.formLogin(AbstractHttpConfigurer::disable)
+			.httpBasic(AbstractHttpConfigurer::disable);
+
+		return http.build();
+	}
+
+	@Bean
+	@Order(2)
+	public SecurityFilterChain filterChain2(HttpSecurity http) throws Exception {
 		http
 			.csrf(AbstractHttpConfigurer::disable)
 			.authorizeHttpRequests(auth -> auth
-				.requestMatchers("/", "/resources/**", "/css/**", "/js/**", "/images/**", "/error", "/api/**", "/icons/**", "/module/**").permitAll()
-				.requestMatchers(publicUris).permitAll()
 				.anyRequest().authenticated()
 			)
+			.addFilterBefore(new AuthFilter(proxyService), UsernamePasswordAuthenticationFilter.class)
 			.formLogin(AbstractHttpConfigurer::disable)
 			.httpBasic(AbstractHttpConfigurer::disable);
 
