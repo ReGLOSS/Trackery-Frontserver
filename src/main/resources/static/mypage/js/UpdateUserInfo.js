a9912e92379506800864c2c62a5293e59c045915
durururuk <147094944+durururuk@users.noreply.github.com>
2025-04-11 01:43:12 +0900

    feature/마이페이지/ 닉네임 수정 기능 작성

--- src/main/resources/static/mypage/js/UpdateUserInfo.js
+++ src/main/resources/static/mypage/js/UpdateUserInfo.js
@@ -44,6 +44,49 @@ document.getElementById("updatePasswordSubmitBtn").addEventListener("click", fun
         }
     })
 })
+//유저명 변경 인풋 디바운스 관련
+
+
+//닉네임 변경 인풋 디바운스 관련
+const updateNicknameInputForm = document.getElementById("updateNicknameInputForm");
+const updateNicknameSubmitBtn = document.getElementById("updateNicknameSubmitBtn");
+
+updateNicknameInputForm.addEventListener("input", debounce(
+        () => applyValidationClass(updateNicknameInputForm,
+            (updateNicknameInputForm.value.trim() !== "" && updateNicknameInputForm.value !== document.getElementById("presentNicknameInputForm").value),
+            updateNicknameRequiredInputs,
+            updateNicknameSubmitBtn
+        )
+    )
+);
+
+const updateNicknameRequiredInputs = [updateNicknameInputForm];
+
+updateNicknameSubmitBtn.addEventListener("click", function() {
+    fetch("/api/users/me/nickname",
+        {
+            method: "PATCH",
+            credentials: "include",
+            headers: {
+                "Content-Type": "application/json"
+            },
+            body: JSON.stringify({
+                "nickname": updateNicknameInputForm.value
+            })
+        }).then(response => {
+        if (response.status === 200) {
+            alert("비밀번호가 변경되었습니다.");
+            document.getElementsByClassName("update-password-block")[0].style.display = "none";
+        } else {
+            response.json().then(data => {
+                alert(data.message);
+            })
+        }
+    })
+})
+
+
+//비밀번호 변경 인풋 디바운스 관련
 const presentPasswordInputForm = document.getElementById("presentPasswordInputForm");
 const updateNewPasswordInputForm = document.getElementById("updateNewPasswordInputForm");
 const updateNewPasswordConfirmInputForm = document.getElementById("updateNewPasswordConfirmInputForm");
@@ -51,23 +94,38 @@ const updatePasswordSubmitBtn = document.getElementById("updatePasswordSubmitBtn
 
 presentPasswordInputForm
     .addEventListener("input", debounce(
-            () => applyValidationClass(presentPasswordInputForm.closest(".input-group"), presentPasswordInputForm.value.length > 0
-            )));
+            () => applyValidationClass(presentPasswordInputForm.closest(".input-group"),
+                presentPasswordInputForm.value.length > 0,
+                updatePasswordRequiredInputs,
+                updatePasswordSubmitBtn
+            )
+        )
+    );
 
 updateNewPasswordInputForm
     .addEventListener("input", debounce(
-        () => applyValidationClass(updateNewPasswordInputForm.closest(".input-group"), validatePassword(updateNewPasswordInputForm.value)
-        )));
+            () => applyValidationClass(
+                updateNewPasswordInputForm.closest(".input-group"),
+                validatePassword(updateNewPasswordInputForm.value),
+                updatePasswordRequiredInputs,
+                updatePasswordSubmitBtn)
+        )
+    );
 
 updateNewPasswordConfirmInputForm
     .addEventListener("input", debounce(
-        () => applyValidationClass(updateNewPasswordConfirmInputForm.closest(".input-group"), updateNewPasswordConfirmInputForm.value === updateNewPasswordInputForm.value
-        )));
+            () => applyValidationClass(
+                updateNewPasswordConfirmInputForm.closest(".input-group"),
+                updateNewPasswordConfirmInputForm.value === updateNewPasswordInputForm.value,
+                updatePasswordRequiredInputs,
+                updatePasswordSubmitBtn)
+        )
+    );
 
 
-function applyValidationClass(input, isValid) {
+function applyValidationClass(input, isValid, requiredInputs, button) {
     toggleValidationClass(input, isValid);
-    checkRequiredFields();
+    checkRequiredFields(requiredInputs, button);
 }
 
 function toggleValidationClass(input, isValid) {
@@ -75,11 +133,31 @@ function toggleValidationClass(input, isValid) {
     input.classList.toggle("is-invalid", !isValid);
 }
 
-const requiredInputs = [presentPasswordInputForm.closest(".input-group"), updateNewPasswordInputForm.closest(".input-group"), updateNewPasswordConfirmInputForm.closest(".input-group")];
+const updatePasswordRequiredInputs = [presentPasswordInputForm.closest(".input-group"), updateNewPasswordInputForm.closest(".input-group"), updateNewPasswordConfirmInputForm.closest(".input-group")];
 
-function checkRequiredFields() {
+function checkRequiredFields(requiredInputs, button) {
     const allValid = requiredInputs.every(input => input.classList.contains("is-valid"));
-    updatePasswordSubmitBtn.disabled = !allValid;
+    button.disabled = !allValid;
+}
+
+document.getElementById("editPasswordBtn").addEventListener("click", function () {
+    toggleBlock(document.getElementsByClassName("update-password-block")[0]);
+})
+
+document.getElementById("editUsernameBtn").addEventListener("click", function () {
+    toggleBlock(document.getElementsByClassName("update-username-block")[0]);
+})
+
+document.getElementById("editNicknameBtn").addEventListener("click", function () {
+    toggleBlock(document.getElementsByClassName("update-nickname-block")[0]);
+})
+
+function toggleBlock(blockElement) {
+    if (blockElement.style.display === "none") {
+        blockElement.style.display = "flex";
+    } else {
+        blockElement.style.display = "none";
+    }
 }
 
 
