92160be0e22b0f416b2e4eafd2525d2927c62fb1
durururuk <147094944+durururuk@users.noreply.github.com>
2025-07-08 16:42:03 +0900

    refactor/image/ 업로드페이지에서 위치 정보와 날짜 정보 중 하나라도 없으면 전부 없음 처리되는 문제 수정

--- src/main/resources/static/upload/js/ExifParser.js
+++ src/main/resources/static/upload/js/ExifParser.js
@@ -6,19 +6,30 @@ export function parseExif(file) {
                 const latRef = EXIF.getTag(this, "GPSLatitudeRef");
                 const lng = EXIF.getTag(this, "GPSLongitude");
                 const lngRef = EXIF.getTag(this, "GPSLongitudeRef");
-                const dateTime = EXIF.getTag(this, "DateTimeOriginal");
+                const dateTime = EXIF.getTag(this, "DateTimeOriginal") || EXIF.getTag(this, "DateTime");
 
-                if (lat && lng && latRef && lngRef && dateTime) {
+                // GPS 정보가 모두 있는 경우
+                if (lat && lng && latRef && lngRef) {
                     const latitude = convertDMSToDD(lat, latRef);
                     const longitude = convertDMSToDD(lng, lngRef);
 
                     resolve({
                         latitude,
                         longitude,
+                        dateTime: dateTime || null
+                    });
+                }
+                // GPS 정보는 없지만 촬영 날짜가 있는 경우
+                else if (dateTime) {
+                    resolve({
+                        latitude: null,
+                        longitude: null,
                         dateTime
                     });
-                } else {
-                    resolve(null); // 값이 없을 경우 null 반환
+                }
+                // 아무 정보도 없는 경우
+                else {
+                    resolve(null);
                 }
             } catch (err) {
                 reject(err);
